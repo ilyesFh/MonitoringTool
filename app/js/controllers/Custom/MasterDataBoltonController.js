95424b65b6696893fa7bfe751580d26b7a2d1a8f
MarouenHamdounAvaxia <marouen.hamdoun@gmail.com>
2017-08-15 11:33:06 +0900

    google key

--- app/js/controllers/Custom/MasterDataBoltonController.js
+++ app/js/controllers/Custom/MasterDataBoltonController.js
@@ -228,7 +228,7 @@ app.controller('ErrorDetailsController', function($scope , $http, $filter , $sta
 
 	$scope.exportData = function () {
 
-		$scope.queryExport = 'SELECT Entry1 as Interface_Name, Entry2 as File_Count, Entry3 as Records_in_MYSQL, case when Entry2 = Entry3 then \'Synced\' else \'Not Synced\' end as Status INTO XLSX("Report_Pricing_MySQLToBolton.xlsx",{}) FROM ?';
+		$scope.queryExport = 'SELECT Entry4 as Interface_Name, Entry2 as File_Count, Entry3 as Records_in_MYSQL, case when Entry2 = Entry3 then \'Synced\' else \'Not Synced\' end as Status INTO XLSX("Report_Pricing_MySQLToBolton.xlsx",{}) FROM ?';
 
 		console.log($scope.queryExport);
 		alasql($scope.queryExport, [$scope.mdRecordsArray]);

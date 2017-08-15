92f340f09f37352a78b1d9fdeded14af370030ed
MarouenHamdounAvaxia <marouen.hamdoun@gmail.com>
2017-08-15 15:53:32 +0900

    icon

--- app/js/controllers/Custom/TruckStockController.js
+++ app/js/controllers/Custom/TruckStockController.js
@@ -22,9 +22,8 @@ app.controller('TruckStockController', function ($scope, $http, $filter , $state
 	$scope.map = { center: { latitude: 35.652832, longitude: 139.839478 }, zoom: 10 };
 	//$scope.bounds = { northeast : { latitude: 71.7593, longitude: -26.5791 }, southwest : { latitude: -0.6621, longitude: -158.4150 } };
 	
-	// Map
-  
-  var Markers = [
+	/*
+  $scope.markers = [
     {
       "id": "0",
       "coords": {
@@ -33,7 +32,11 @@ app.controller('TruckStockController', function ($scope, $http, $filter , $state
       },
       "window": {
         "title": "JW71"
-      }
+      },
+	  options: { draggable: false,
+      icon:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'}
+	  
+      
     },
     {
       "id": "1",
@@ -46,8 +49,10 @@ app.controller('TruckStockController', function ($scope, $http, $filter , $state
       }
     }
   ];
+  
+  */
 	
-	$scope.markers = Markers;
+	//$scope.markers = Markers;
 	
 	$scope.circles = [
             {
@@ -76,13 +81,7 @@ app.controller('TruckStockController', function ($scope, $http, $filter , $state
         ];
 	
 
-	$scope.options = {
-		animate: false,
-		barColor: '#2C3E50',
-		scaleColor: false,
-		lineWidth: 20,
-		lineCap: 'circle'
-	};
+	
 
 	$scope.load = function () {
 		console.log("load event detected!");
@@ -94,15 +93,24 @@ app.controller('TruckStockController', function ($scope, $http, $filter , $state
 		var res = $http.post('http://117.55.209.110:9080/ws/simple/getMysqlTest;boomi_auth=YXZheGlhLTlGQ0pJRjo3ZDA1NzAwZC1mODM1LTQ4NTUtOThjNC03OWFlMTc1OGRkYWI=', msgdata).
 			then(function (response) {
 				console.log(response.data[0][0]);
-
 				$scope.allMdRecords = response.data[0][0];
 				$scope.truckStockList = response.data[0][0];
 				
+				//$scope.yesterday = $scope.TodayDate - 1;
+				//Web Service Call For Map
+				$scope.yesterday = "20170814"
+				$scope.formattedDate = $filter('date')($scope.TodayDate, "yyyyMMdd");
+				var msgdata = "{\"Var1\": " + "\"" + $scope.yesterday + "\", \"Prefix\":\"TruckStockMap\"  }";
+				var res = $http.post('http://117.55.209.110:9080/ws/simple/getMysqlTest;boomi_auth=YXZheGlhLTlGQ0pJRjo3ZDA1NzAwZC1mODM1LTQ4NTUtOThjNC03OWFlMTc1OGRkYWI=', msgdata).
+					then(function (response) {
+					console.log(response.data[0]);
+					$scope.markers = response.data[0];
+					
+					});
 
 			});
 	}
 
-	$scope.optionsChart = "{ easing: 'easeOutBounce', barColor: 'orange', trackColor: '#f5f5f5', scaleColor: '#eaeaea', lineCap: 'square', lineWidth: 15, size: 130,animate: 1000,percent: 66.7 }"
 
 		// Post Web CALL
 		$scope.postMessage = function () {
@@ -252,46 +260,4 @@ $scope.dtpick = {
   
 
 });
-
-app.controller('JVectorMapDemoCtrl', ['$scope', function($scope) {
-    $scope.world_markers = [
-      {latLng: [41.90, 12.45], name: 'Vatican City'},
-      {latLng: [43.73, 7.41], name: 'Monaco'},
-      {latLng: [-0.52, 166.93], name: 'Nauru'},
-      {latLng: [-8.51, 179.21], name: 'Tuvalu'},
-      {latLng: [43.93, 12.46], name: 'San Marino'},
-      {latLng: [47.14, 9.52], name: 'Liechtenstein'},
-      {latLng: [7.11, 171.06], name: 'Marshall Islands'},
-      {latLng: [17.3, -62.73], name: 'Saint Kitts and Nevis'},
-      {latLng: [3.2, 73.22], name: 'Maldives'},
-      {latLng: [35.88, 14.5], name: 'Malta'},
-      {latLng: [12.05, -61.75], name: 'Grenada'},
-      {latLng: [13.16, -61.23], name: 'Saint Vincent and the Grenadines'},
-      {latLng: [13.16, -59.55], name: 'Barbados'},
-      {latLng: [17.11, -61.85], name: 'Antigua and Barbuda'},
-      {latLng: [-4.61, 55.45], name: 'Seychelles'},
-      {latLng: [7.35, 134.46], name: 'Palau'},
-      {latLng: [42.5, 1.51], name: 'Andorra'},
-      {latLng: [14.01, -60.98], name: 'Saint Lucia'},
-      {latLng: [6.91, 158.18], name: 'Federated States of Micronesia'},
-      {latLng: [1.3, 103.8], name: 'Singapore'},
-      {latLng: [1.46, 173.03], name: 'Kiribati'},
-      {latLng: [-21.13, -175.2], name: 'Tonga'},
-      {latLng: [15.3, -61.38], name: 'Dominica'},
-      {latLng: [-20.2, 57.5], name: 'Mauritius'},
-      {latLng: [26.02, 50.55], name: 'Bahrain'},
-      {latLng: [0.33, 6.73], name: 'São Tomé and Príncipe'}
-    ];
-
-    $scope.usa_markers = [
-      {latLng: [40.71, -74.00], name: 'New York'},
-      {latLng: [34.05, -118.24], name: 'Los Angeles'},
-      {latLng: [41.87, -87.62], name: 'Chicago'},
-      {latLng: [29.76, -95.36], name: 'Houston'},
-      {latLng: [39.95, -75.16], name: 'Philadelphia'},
-      {latLng: [38.90, -77.03], name: 'Washington'},
-      {latLng: [37.36, -122.03], name: 'Silicon Valley'}
-    ];
-  }])
-;
 
